b02a69390a03fc94d00b1e689d20989db4aaf57c
Nate <ncggardner@gmail.com>
2020-04-08 17:04:01 -0400

    commit for coaching deliverable 2

---
+++ Deliverable2/Deliverable2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word to be encoded");
+            string input = Console.ReadLine().ToUpper();
+            for(int i =0;i<input.Length;i++)
+            {
+                char letter = input[i];
+                char c = letter;
+                int ascii = c;
+                Console.Write(ascii + "-");
+                
+                
+
+                   
+                
+                
+            }
+            
+
+    
+
+        }
+    }
+}
